04323baef00e5d4052e52dd021aa33c794813db5
Anurup Raveendran <anurupraveendran@gmail.com>
2020-09-14 05:08:01 +0530

    Implemented Profile Component

---
+++ src/components/User/Profile.vue
@@ -0,0 +1,43 @@
+<template>
+    <div class="box" id="profile">
+        <div><img :src="user.gravatar" ></div>
+        <span class="name">{{ user.name }}</span>
+        <span class="location">{{ user.location }}</span>
+    </div>
+</template>
+<script>
+export default {
+    name: 'Profile',    
+    data() {
+        return {
+            user: this.$store.getters.getCurrentUser
+        }
+    }
+}
+</script>
+<style scoped>
+    #profile {
+        width: 100%;
+    }
+
+    #profile > span {
+        display: block;
+        padding-top: 0.5rem;
+        padding-bottom: 0.5rem;
+        font-family:-apple-system, BlinkMacSystemFont, 'Segoe UI', Roboto, Oxygen, Ubuntu, Cantarell, 'Open Sans', 'Helvetica Neue', sans-serif;
+    }
+
+    .name {        
+        font-weight: bold;
+        font-size: 2.5rem;        
+    }
+
+    /* .location {    } */
+
+    #profile img {
+        border-radius: 150px;
+        border: 1px solid #aaa;       
+        /* box-shadow: 0 0px 5px 0 rgba(0, 0, 0, 0.2); */
+        max-width: 120px;
+    }
+</style>
