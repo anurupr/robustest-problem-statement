29f5d654ede3af937e6c06052022b49e57809f6a
Anurup Raveendran <anurupraveendran@gmail.com>
2020-09-19 13:35:26 +0530

    - NewsFeed component was being mounted, so it was calling loadPosts which gave a warning while unit testing Home. Putting it as a jest stub fixed it . also using shallow mount removes the warning

--- tests/unit/Home.spec.js
+++ tests/unit/Home.spec.js
@@ -1,8 +1,9 @@
-import { mount, createLocalVue } from '@vue/test-utils'
+import { mount, shallowMount, createLocalVue } from '@vue/test-utils'
 import VueRouter from 'vue-router'
 import Vuex from 'vuex'
 
 import Home from '@/views/Home'
+import NewsFeed from '@/components/Social/NewsFeed'
 import Column from '@/components/Common/Layout/Column'
 import Row from '@/components/Common/Layout/Row'
 import Container from '@/components/Common/Layout/Container'
@@ -18,6 +19,10 @@ const factory = (opts = {}) => {
     return mount(Home, opts)
 }
 
+const sfactory = (opts = {}) => {
+    return shallowMount(Home, opts)
+}
+
 const router = new VueRouter({
     mode: 'history'
   })
@@ -25,6 +30,7 @@ const router = new VueRouter({
 describe('Home', () => {
     let store
     let state
+    let actions
 
     beforeEach(() => {
         state = {
@@ -32,20 +38,37 @@ describe('Home', () => {
             posts: []
         }
 
+        actions = {
+            loadPosts: jest.fn()
+        }
+
         store = new Vuex.Store({
-            state
+            state,
+            actions
         })
     })
 
-    it('renders container', () => {
+    it('renders container (shallowMount) without NewsFeed Component rendered - loadPosts not called', () => {
+        const wrapper = sfactory({
+            store,
+            localVue,
+            // router
+        })
+        // container component will not be rendered, there will only be a stub
+        expect(wrapper.find('.container').exists()).toBe(false)
+        // use findComponent instead
+        expect(wrapper.findComponent(Container).exists()).toBe(true)
+    })
+
+    it('renders container (mount) with NewsFeed component and action loadPosts should be called', async () => {
         const wrapper = factory({
           store,
           localVue,
           router
         });
-        console.log(wrapper.html())
         expect(wrapper.find('.container').exists()).toBe(true)
+        expect(wrapper.findComponent(NewsFeed).exists()).toBe(true)
+        await localVue.nextTick()
+        expect(actions.loadPosts).toHaveBeenCalled()
     })
-
-
 })
