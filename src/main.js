e5754ce083dccdee6dc8fdf303a11ea5857388cf
Anurup Raveendran <anurupraveendran@gmail.com>
2020-10-07 02:18:11 +0530

    Reset.css not required, vuetify applies its own reset global.css -- added background

--- src/main.js
+++ src/main.js
@@ -2,7 +2,6 @@ import Vue from 'vue'
 import App from './App.vue'
 import store from '@/store'
 import router from '@/router'
-import './assets/reset.css'
 import './assets/global.css'
 
 import { FontAwesomeIcon, FontAwesomeLayers, FontAwesomeLayersText } from '@fortawesome/vue-fontawesome'
