45785ae636bf7af4d9ee288c2778dafc5efcc8ab
Anurup Raveendran <anurupraveendran@gmail.com>
2020-10-07 15:49:29 +0530

    Profile spec

---
+++ tests/unit/Profile.spec.js
@@ -0,0 +1,61 @@
+/* NOTE: 
+   Tests pass with error
+    [Vuetify] Multiple instances of Vue detected
+    See https://github.com/vuetifyjs/vuetify/issues/4068
+
+    If you're seeing "$attrs is readonly", it's caused by this
+ */
+
+import Vuetify from 'vuetify'
+import { mount, createLocalVue } from '@vue/test-utils'
+import Profile from '@/components/User/Profile'
+import Vuex from 'vuex'
+import { cgravatar } from '@/utils'
+
+const localVue = createLocalVue()
+localVue.use(Vuetify)
+localVue.use(Vuex)
+
+const factory = (opts = {}) => {
+  return mount(Profile, opts)
+}
+
+describe('Profile', () => {
+    let vuetify
+    let getters 
+    let store
+
+    beforeEach(() => {
+      vuetify = new Vuetify()
+
+      getters = {
+        getCurrentUser: () => {
+          return {
+            id: 1,
+            username: 'rtester',
+            name: 'Robus Tester',
+            email: 'robustester@gmail.com',
+            location: 'Bangalore, India',
+            gravatar: cgravatar('robustester@gmail.com')
+          }
+        }
+      }
+
+      store = new Vuex.Store({
+          getters
+      })
+    })
+
+    it('renders profile with avatar', async () => {
+      const wrapper = factory({
+        localVue,
+        vuetify,
+        store
+      })
+
+      expect(wrapper.find('.profile').exists()).toBe(true)
+      expect(wrapper.find('.v-avatar').exists()).toBe(true)
+      // TODO test with avatar image
+    })
+   
+})
